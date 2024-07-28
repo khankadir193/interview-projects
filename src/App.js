e344a9b6dd92198e88867bbb7942e7382abd4479
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-28 19:32:09 +0530

    puted the css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
       <AccordianComp name="AccordionComponent" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."/>
+      <AccordianComp name="second Accordion comp" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."/>
     </div>
   );
 }

6550bbb573e5bc02a8027bf51612d80462c948e8
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-28 20:51:08 +0530

    getting profile img in dynamic

--- src/GitHubProfile/GitHubProfileFinder.js
+++ src/GitHubProfile/GitHubProfileFinder.js
@@ -3,26 +3,44 @@ import React, { useEffect, useState } from 'react'
 import './githubProfile.css';
 
 const GitHubProfileFinder = () => {
-    const [profileData,setProfileData] = useState();
+    const [profileData, setProfileData] = useState();
+    const [inputValue, setInputValue] = useState();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const username = 'khankadir193'
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const username = 'khankadir193'
+    //         const response = await axios.get(`https://api.github.com/users/${username}`);
+    //         console.log('response...//', response.data);
+    //         setProfileData(response.data);
+    //     }
+    //     fetchData();
+    // }, [0]);
 
+    const handleSubmit = async() => {
+        console.log("inputValue...",inputValue);
+        if (inputValue) {
+            const username = inputValue
             const response = await axios.get(`https://api.github.com/users/${username}`);
             console.log('response...//', response.data);
             setProfileData(response.data);
-        }
-        fetchData();
-    },[0]);
-    return (
-        <div className='profile-container'>
-            <img src={profileData?.avatar_url} alt='gitHub profileImage' />
-            <p>{profileData?.login}</p>
-            <h3>{profileData?.name}</h3>
-            <h3>{profileData?.bio}</h3>
-        </div>
-    )
-}
+        };
+    }
 
-export default GitHubProfileFinder;
+        const handleChange = (e) => {
+            console.log('e...', e.target.value);
+            setInputValue(e.target.value);
+        }
+
+        return (
+            <div className='profile-container'>
+                <img src={profileData?.avatar_url} alt='gitHub profileImage' />
+                <p>{profileData?.login}</p>
+                <h3>{profileData?.name}</h3>
+                <h3>{profileData?.bio}</h3>
+                <input type='text' placeholder='enter your github username' onChange={handleChange} />
+                <button onClick={handleSubmit}>Submit</button>
+            </div>
+        )
+    }
+
+    export default GitHubProfileFinder;

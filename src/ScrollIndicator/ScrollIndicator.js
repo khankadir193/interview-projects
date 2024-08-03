1a0f120074929a09c139e2b5c33933b5d992ab9a
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-08-03 23:03:16 +0530

    refactor fetchData function

--- src/ScrollIndicator/ScrollIndicator.js
+++ src/ScrollIndicator/ScrollIndicator.js
@@ -3,55 +3,49 @@ import './Scroll.css';
 import ProgressBar from './ProgressBar';
 
 const ScrollIndicator = () => {
-  const [data, setData] = useState([]);
-  const [scrollPercentage, setScrollPercentage] = useState(0);
+    const [data, setData] = useState([]);
+    const [scrollPercentage, setScrollPercentage] = useState(0);
 
-  const handleScroll = async () => {
-    try {
-      const winScroll = document.documentElement.scrollTop;
-      const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-      const progress = (winScroll / height) * 100;
-      const scrollPercentage = Math.round(progress);
-      console.log('scrollPercentage..', scrollPercentage);
-      setScrollPercentage(scrollPercentage);
-    } catch (error) {
-      console.error('Error handling scroll:', error);
+    const handleScroll = async () => {
+        try {
+            const winScroll = document.documentElement.scrollTop;
+            const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+            const progress = (winScroll / height) * 100;
+            const scrollPercentage = Math.round(progress);
+            console.log('scrollPercentage..', scrollPercentage);
+            setScrollPercentage(scrollPercentage);
+        } catch (error) {
+            console.error('Error handling scroll:', error);
+        }
     }
-  }
 
 
-  useEffect(() => {
-    document.addEventListener('scroll',handleScroll);
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://dummyjson.com/products?limit=100');
-        const res = await response.json();
-        setData(res.products);
-      } catch (err) {
-        console.log('Error fetching data:', err);
-      }
-    };
+    useEffect(() => {
+        document.addEventListener('scroll', handleScroll);
+        const fetchData = async () => {
+            const res = await (await fetch('https://dummyjson.com/products?limit=100')).json();
+            setData(res.products);
+        };
+        fetchData();
+    }, []);
 
-    fetchData();
-  }, []);
+    if (data.length === 0) {
+        return null;
+    }
 
-  if (data.length === 0) {
-    return null;
-  }
-
-  return (
-    <>
-      <ProgressBar progress={scrollPercentage} />
-      <div className='scroll-container'>
-        <h1>title</h1>
-        <div style={{ height: 'calc(100vh - 30px)' }}>
-          {data.map((item) => (
-            <h3 key={item.id}>{item.title}</h3>
-          ))}
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <ProgressBar progress={scrollPercentage} />
+            <div className='scroll-container'>
+                <h1>title</h1>
+                <div style={{ height: 'calc(100vh - 30px)' }}>
+                    {data.map((item) => (
+                        <h3 key={item.id}>{item.title}</h3>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 };
 
 

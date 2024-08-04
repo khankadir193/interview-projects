d0fe43ee1c7476f07f61c1c092a8f8ec8c884648
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-08-04 19:48:06 +0530

    tic tac toe game

--- src/TicTacToe/ButtonComp.js
+++ src/TicTacToe/ButtonComp.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './ticTacToe.css';
 
-const ButtonComp = ({value,board}) => {
+
+const ButtonComp = ({ value, board , handleClick}) => {
+  // if (!value) {
+  //   console.error('ButtonComp: value prop is missing or null');
+  //   return null;
+  // }
+
+  // if (!board) {
+  //   console.error('ButtonComp: board prop is missing or null');
+  //   return null;
+  // }
+
   return (
     <div>
-        <button>{value}</button>
+      <button onClick={handleClick}>{value}</button>
     </div>
   )
 }
+
 
 export default ButtonComp;
--- src/TicTacToe/TicTacToeApp.js
+++ src/TicTacToe/TicTacToeApp.js
@@ -11,25 +11,43 @@ const TicTacToeApp = () => {
             return;
         }
         const newBoard = [...board];
+        newBoard[index] = isX ? 'X' : 'O';
+        setIsX(!isX);
+        setBoard(newBoard);
+        checkForWinner(newBoard);
     }
 
+    const checkForWinner = (board) => {
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+            [0, 4, 8], [2, 4, 6] // Diagonals
+        ];
+
+        for (const combination of winningCombinations) {
+            const [a, b, c] = combination;
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                setWinner(board[a]);
+                return;
+            }
+        }
+
+        if (board.every(cell => cell !== null)) {
+            setWinner('draw');
+        }
+    }
 
     return(
         <div className="button-container">
-            <ButtonComp value='X' board={board} />
-            <ButtonComp value='X' board={board} />
-            <ButtonComp value='X' board={board} />
-            <ButtonComp value='X' board={board} />
-            <ButtonComp value='X' board={board} />
-            <ButtonComp value='X' board={board} />
-            <ButtonComp value='X' board={board} />
-            <ButtonComp value='X' board={board} />
-            <ButtonComp value='X' board={board} />
-            <ButtonComp value='X' board={board} />
-            <ButtonComp value='X' board={board} />
-            <ButtonComp value='X' board={board} />
-
+            {board.map((_, index) => (
+                <ButtonComp 
+                    key={index} 
+                    value={board[index] || ''} 
+                    handleClick={() => handleClick(index)}
+                />
+            ))}
         </div>
     )
 };
+
 export default TicTacToeApp;

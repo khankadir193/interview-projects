a9c89bb6ba537c96a06b47472c4557918a1bddb8
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-08-10 19:19:07 +0530

    list

--- src/TodoApp/List.js
+++ src/TodoApp/List.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './todos.css';
+import { FontAwesomeIcon } from '@fortawesome/fontawesome-free';
+import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons';
 
-const List = ({data}) => {
-    console.log('data...',data);
+
+function List({ data }) {
+  console.log('data...', data);
   return (
-    <div>{data}</div>
-  )
-}
+    <div className='list-container'>
+      <div className='subList-container'>
+        <h3>{data}</h3>
+        <FontAwesomeIcon icon={faTrashAlt} />
+        <FontAwesomeIcon icon={faEdit} />
+        <FontAwesomeIcon icon={faTrashAlt} />
+        <FontAwesomeIcon icon={faEdit} />
+        {/* <i className="fas fa-trash-alt"></i>
+        <i className="fas fa-edit"></i>
+        <i class="fas fa-trash-alt"></i>
+        <i class="fas fa-edit"></i> */}
+      </div>
+    </div>
+  );
+};
 
-export default List
+export default List;

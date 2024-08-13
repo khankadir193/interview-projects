c8af01273b816cdf4fd35f9c65948a40cad5433e
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-08-13 09:42:59 +0530

    editable is working fine

--- src/TodoApp/List.js
+++ src/TodoApp/List.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './todos.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+// import {ContentEditable,suppressContentEditableWarning} from 'react-contenteditable';
 
 
 function List({ item }) {
+  const [edit,setEdit] = useState(false);
+  const handleEdit = () => {
+    console.log('edit...');
+    setEdit(true);
+  };
+
+  const handleSave = (e) => {
+    console.log('save...', e.target.innerText);
+    setEdit(false);
+  }
   console.log('data...', item);
   return (
     <div className='list-container'>
       <div className='subList-container'>
-        <span>{item}</span>
+        <span
+          contentEditable={edit}
+          suppressContentEditableWarning={true}
+          onBlur={handleSave}
+        >
+          {item}
+        </span>
         <div className='icons'>
-          <FontAwesomeIcon icon={faEdit} />
+          <FontAwesomeIcon icon={faEdit} onClick={handleEdit} />
           <FontAwesomeIcon icon={faTrashAlt} />
         </div>
 

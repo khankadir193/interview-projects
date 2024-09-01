806fd4b41d80fd28e49ccff7d57d73b45d873703
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-09-01 17:51:16 +0530

    quiz functionality

--- src/QuizApp/QuizComp.js
+++ src/QuizApp/QuizComp.js
@@ -4,19 +4,27 @@ import QuizData from './quizData.json';
 import QuizQuestions from './QuizQuestions';
 
 const QuizComp = () => {
-    const [submit,setSubmit] = useState();
+    const [submit, setSubmit] = useState([]);
+    const [quizData, setQuizData] = useState([QuizData.quizData[0]]);
     // console.log('quiz-data...', QuizData.quizData);
 
-    const handleSubmit = ()=>{
-        console.log('handleSumbit method.....');
-    }
+    const handleSubmit = (option) => {
+        if (QuizData.quizData.length - 1 === submit?.length) {
+            alert('Quiz Completed');
+            setQuizData([QuizData.quizData[0]]);
+            return
+        } else {
+            setQuizData([QuizData.quizData[submit.length + 1]]);
+            setSubmit([...submit, option]);
+        }
+    };
 
 
     return (
         <div className='quiz-container'>
             <div className='card-container'>
                 <h1>Quiz App</h1>
-                {QuizData.quizData.map((item, index) => {
+                {quizData.map((item, index) => {
                     return (
                         <div id={index}>
                             <QuizQuestions

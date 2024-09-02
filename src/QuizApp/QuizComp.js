0471e7bbdec5de06f4669db7cbcca5444acdaffd
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-09-02 10:59:05 +0530

    quizComp logic

--- src/QuizApp/QuizComp.js
+++ src/QuizApp/QuizComp.js
@@ -6,17 +6,34 @@ import QuizQuestions from './QuizQuestions';
 const QuizComp = () => {
     const [submit, setSubmit] = useState([]);
     const [quizData, setQuizData] = useState([QuizData.quizData[0]]);
+    const [submitData, setSubmitData] = useState([]); // true or false
+    const [rightQues,setRightQues] = useState(0);
     // console.log('quiz-data...', QuizData.quizData);
+
+    const questionEvaluate = (option) => {
+        if (option === QuizData.quizData[submit.length].correctAnswer) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 
     const handleSubmit = (option) => {
         if (QuizData.quizData.length - 1 === submit?.length) {
             alert('Quiz Completed');
             setQuizData([QuizData.quizData[0]]);
             setSubmit([]);
+            const totalRightQuestons = submitData.filter(x => x).length;
+            setRightQues(totalRightQuestons);
+            console.log('totalRightQuestons...',totalRightQuestons);
             return
         } else {
             setQuizData([QuizData.quizData[submit.length + 1]]);
             setSubmit([...submit, option]);
+            const queRes = questionEvaluate(option);
+            // submitData.push(...queRes);
+            setSubmitData([...submitData, queRes]);
+            console.log('submitData...',submitData);
         }
     };
 
@@ -34,7 +51,8 @@ const QuizComp = () => {
                             />
                         </div>
                     );
-                })}
+                })};
+                {rightQues ? <h2>Right Answers: {rightQues}</h2> : null}
             </div>
         </div>
     )

4c1ee230a69b6cc22fa4b795784f7d7440f58ac9
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-09-03 12:01:10 +0530

    locationFinder:

--- src/LocationFinder/LocFinderApp.js
+++ src/LocationFinder/LocFinderApp.js
@@ -1,36 +1,62 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+
 const customIcon = new L.Icon({
   iconUrl: require('./custom-marker.png'),
   iconSize: [35, 35],
 });
+
 const LocFinderApp = () => {
-  const position = [51.505, -0.09];
+  const [position, setPosition] = useState([51.505, -0.09]); // Default position
   const [markers, setMarkers] = useState([position]);
 
+  // Function to add a marker on map click
   const addMarker = (e) => {
-    setMarkers([...markers, [e.latlng.lat, e.latlng.lng]]);
+    const location = { lat: e.latlng.lat, lng: e.latlng.lng };
+    setMarkers([...markers, location]);
+  };
+
+  // Function to get the current location
+  const handleLocationClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          const { latitude, longitude } = pos.coords;
+          const currentLocation = [latitude, longitude];
+          setPosition(currentLocation);
+          setMarkers([currentLocation]); // Add current location as a marker
+        },
+        (err) => {
+          console.error("Error getting location:", err);
+        }
+      );
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
   };
 
   return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      style={{ height: '100vh', width: '100%' }}
-      onClick={addMarker}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      {markers.map((marker, index) => (
-        <Marker key={index} position={marker} icon={customIcon}>
-          <Popup>You clicked at {marker.toString()}</Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <div>
+      <button onClick={handleLocationClick}>Get Current Location</button>
+      <MapContainer
+        center={position}
+        zoom={13}
+        style={{ height: '50vh', width: '50%' }}
+        onClick={addMarker}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {markers.map((marker, index) => (
+          <Marker key={index} position={marker} icon={customIcon}>
+            <Popup>You clicked at {marker.toString()}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
   );
 };
 

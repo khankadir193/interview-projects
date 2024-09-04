0aeef1a2c09c36ff422e38faacf41653ecd32979
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-09-04 10:22:08 +0530

    brush color implemented but  it's not wroking

--- src/PainApp/PaintComp.js
+++ src/PainApp/PaintComp.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import ReactPainter from 'react-painter';
 import './PaintComp.css';
 import { SketchPicker } from 'react-color';
 
 const CustomPainter = () => {
     const [color, setColor] = useState('#000000');
+    useEffect(() => {
+        console.log('coloruseEffect', color);
+        const canvas = document.querySelector('canvas');
+        if(canvas){
+            console.log('canvas...',canvas);
+            const ctx = canvas.getContext('2d');
+            console.log('ctx..????',ctx);
+            ctx.strokeStyle = color;
+            ctx.fillStyle = color;
+            // ctx.fillRect(0, 0, canvas.width, canvas.height);
+        }
+    },[color])
     const handleSave = (blob) => {
         console.log('Saved image:', blob);
         // Here you can handle the saved image blob (e.g., upload it, display it, etc.)
     };
 
     const handleColorChange = (color) => {
-        console.log('color...???',color.hex);
-        setColor(color);
+        // console.log('color...???',color.hex);
+        setColor(color.hex);
         console.log(color.hex); // or do something with the selected color
     };
 
@@ -32,7 +44,7 @@ const CustomPainter = () => {
                         {imageCanDownload && (
                             <p className='errorText'>Sorry, the image that you have provided is not accessible.</p>
                         )}
-                        <div className='canvasContainer'>
+                        <div className='canvasContainer' brushColor={color}>
                             {canvas}
                         </div>
                         <button className='saveButton' type='button' title='Save' aria-label='Save' onClick={triggerSave}>

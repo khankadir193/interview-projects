bfda3c8b61cd8a44ef1ce808047880c63dd948af
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-09-08 21:38:32 +0530

    searchbar

--- src/BlogApp/NavBar.js
+++ src/BlogApp/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className="collapse navbar-collapse d-flex justify-content-end gap-5" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="#">Home</a>
@@ -24,6 +24,10 @@ const NavBar = () => {
                                 <a className="nav-link" href="#">Contact</a>
                             </li>
                         </ul>
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
                     </div>
                 </div>
             </nav>

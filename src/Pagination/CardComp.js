49a075e101c97d2a3eabe1a621ac8bfc7b1360d0
tejaswiniPujari <tejaswinidinkarpujari@gmail.com>
2024-11-29 23:13:20 +0530

    card-components

--- src/Pagination/CardComp.js
+++ src/Pagination/CardComp.js
@@ -1,12 +1,15 @@
 import React from "react";
+import './cardDesign.css';
 
-const CardComp = ({login,avatar_url,profileUrl})=> {
-    console.log('login',login);
+const CardComp = ({ login, avatar_url, profileUrl }) => {
+    console.log('login', login);
     return (
         <div className="card">
-            <h3>{login}</h3>
-            <h3>{avatar_url}</h3>
-            <h3>{profileUrl}</h3>
+            <img src={avatar_url} alt="avatarImg" className="card-image" />
+            <h3 className="card-title">{login}</h3>
+            <a href={profileUrl} className="card-link" target="_blank" rel="noopener noreferrer">
+                View Profile
+            </a>
         </div>
     )
 };

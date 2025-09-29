11431a52f7330d4b6fd5705edc8f900f4c24963f
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2025-09-29 15:21:40 +0530

    changes

---
+++ src/Pagination/PaginationComp.js
@@ -0,0 +1,14 @@
+import React,{useEffect} from 'react';
+
+const PaginationComp = () => {
+
+    useEffect(()=>{
+        const res = fetch('')
+    })
+    
+  return (
+    <div>PaginationComp</div>
+  );
+}
+
+export default PaginationComp;

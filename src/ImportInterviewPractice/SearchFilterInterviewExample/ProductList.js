1da1185136baeb78a6a9c03771f286da3a944fcb
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2025-12-19 20:04:31 +0530

    product List

--- src/ImportInterviewPractice/SearchFilterInterviewExample/ProductList.js
+++ src/ImportInterviewPractice/SearchFilterInterviewExample/ProductList.js
@@ -5,8 +5,8 @@ const ProductList = () => {
     const [errors, setErrors] = useState('');
     const [loading, setLoading] = useState(true);
     const [search, setSearch] = useState('');
-    const [category,setCategory] = useState('all');
-    const [sort,setSort] = useState('');
+    const [category, setCategory] = useState('all');
+    const [sort, setSort] = useState('');
 
     useEffect(() => {
         const fetchData = async () => {
@@ -36,20 +36,22 @@ const ProductList = () => {
             )
         };
 
-        if(category !== 'all'){
+        if (category !== 'all') {
             result = result.filter(p => p.category === category)
         };
 
 
+        console.log('sort...', sort);
 
-
-
-
+        if (sort === 'asc') {
+            result.sort((a,b)=> a.price-b.price);
+        }else if(sort === 'des'){
+            result.sort((a,b)=> b.price-a.price);
+        }
 
         return result;
-
         // filter
-    }, [search, products,category])
+    }, [search, products, category, sort])
 
 
     // const searchProducts = useMemo(() => {
@@ -60,10 +62,10 @@ const ProductList = () => {
     //         ));
     // }, [search]);
 
-    const categories = useMemo(()=>{
-        return ['all',...new Set(products.map(p=> p.category))];
+    const categories = useMemo(() => {
+        return ['all', ...new Set(products.map(p => p.category))];
         // return ["all", ...new Set(products.map(p => p.category))];
-    },[products])
+    }, [products])
 
     if (errors) return <p>{errors}</p>
     if (loading) return <p>Loading...</p>
@@ -73,10 +75,16 @@ const ProductList = () => {
         <div>
             <input type='text' placeholder='search your text' onChange={e => setSearch(e.target.value)} />
 
-            <select value={category} onChange={e=> setCategory(e.target.value)}>
-                {categories.map(cat=>(
-                        <option value={cat} key={cat}>{cat}</option>
-                    ))}
+            <select value={category} onChange={e => setCategory(e.target.value)}>
+                {categories.map(cat => (
+                    <option value={cat} key={cat}>{cat}</option>
+                ))}
+            </select>
+
+            <select value={sort} onChange={e => setSort(e.target.value)}>
+                <option value=''>Sort By</option>
+                <option value='asc'>Ascending</option>
+                <option value='des'>Descending</option>
             </select>
 
             {
@@ -86,7 +94,7 @@ const ProductList = () => {
                     <ul>
                         {
                             filteredProducts.map(product => (<li key={product.id}>
-                                <strong>{product.title}</strong> - {product.price}
+                                <strong>{product.title}</strong> - ₹ {product.price}
                             </li>)
                             )
                         }
